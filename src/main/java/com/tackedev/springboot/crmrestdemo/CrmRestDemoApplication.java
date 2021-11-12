b49e06e9578c380ab3b0157eced3372f6bf18347
tackedev <quangky.it@gmail.com>
2021-11-12 21:32:28 +0700

    first commit

---
+++ src/main/java/com/tackedev/springboot/crmrestdemo/CrmRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tackedev.springboot.crmrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmRestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tackedev/springboot/crmrestdemo/CrmRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tackedev.springboot.crmrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
